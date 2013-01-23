f1c85ad13f868c35a15a33156ea11c0dffb69aae
Bhagavan Das <das@smartspike.co.uk>
2013-01-23 20:35:44 +0000

    Dbdeploy for mysql with maven build

---
+++ 002_insert_data.sql
@@ -0,0 +1,9 @@
+-- dbdeploy will wrap the application of each change script
+-- in a transaction
+--
+-- DML statements (INSERT, UPDATE etc) can be applied transactionally,
+-- so therefore dbdeploy will ensure that either the whole of this script applies
+-- or none of it does.
+--               -- List the name of all the databases in this server
+
+   select * from Test;
